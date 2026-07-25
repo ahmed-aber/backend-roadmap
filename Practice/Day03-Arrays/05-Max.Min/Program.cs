7a9e1b993dc8815dfb74ced8d0ac67795912c4d9
Ahmed Aber <engahmedaber10@gmail.com>
2026-07-25 16:11:08 +0300

    Complete Day3

---
+++ Practice/Day03-Arrays/05-Max.Min/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] numbers = new int[5] { 450, 552, 655, 5555, 855 };
+int max = 0;
+max = numbers[0];
+
+int min = 0;
+min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+Console.WriteLine("Max value is: {0}", max);
+Console.WriteLine("Min value is: {0}", min);
---
+++ Practice/Day03-Arrays/06-Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = new int[5] { 50, 10, 33, 44, 12 };
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine("Sum of all elements is: {0}", sum);
